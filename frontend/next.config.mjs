c8d9ae68bb0434d5b754d609ef3c4456eb03e0a8
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-12-29 10:02:30 +0100

    Initial commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "export",
+  assetPrefix: "./",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,6 +11,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
